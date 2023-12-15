60f25e185005c8ecc71c4cde7f8a43cbacd54865
UltimateBoss80 <144810378+UltimateBoss80@users.noreply.github.com>
2023-12-15 00:13:46 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,64 +1,62 @@
-/*
-    This program illustrates the use of hash tables and files"
-    Jeremy White
-    12/3/2023
-*/
-
 #include <iostream>
-#include "Dogs.h"
-
+#include <stack>
+#include <queue>
+#include <cstring>
 
 using namespace std;
+
+bool palRecursive(char str[], int f, int l)
+{
+    if(f==l)
+        return true;
+
+    if(str[f]!=str[l])
+        return false;
+
+    return palRecursive(str, f + 1, l - 1);
+
+}
+
+
+
+bool isPalindrome(char str [])
+{
+    int length = strlen(str);
+
+    if (length == 0)
+        return true;
+
+    return palRecursive(str, 0, length - 1);
+
+
+}
 
 int main()
 {
     try
-
     {
-        int choice;
 
-
-        Dogs dogs;
-        do
+        const int length = 100;
+        char word[length];
+        cout << "Enter a palindrome: ";
+        cin >> word;
+        char letter = word[0];
+        if (!isalpha(letter))
         {
-            cout << "Select 1 to see total number of dogs in the world."<< endl;
-            cout << "Select 2 to find out how many dogs are in a specific country." << endl;
-            cout << "Select 3 to quit." << endl;
-            cin >> choice;
+            cout << endl << "The fist character is not a letter, please try again." << endl;
+            cout << "Enter a palindrome: ";
+            cin >> word;
+        }
 
-            switch (choice)
-            {
+        if (isPalindrome(word))
+        {
+            cout << "This word is a palindrome.";
+        }
 
-                case 1:
-                    cout << dogs.getDogPop() << " dogs in the world" << endl;
-                    break;
-
-                case 2:
-                    string country;
-                    cout << "Enter country: ";
-                    cin >> country;
-
-                    if (dogs.countryAvailable(country))
-                    {
-                        cout << dogs.getCountryPop(country) << " dogs in " << country << endl;
-                    }
-                    else
-                    {
-                        throw "Country not found";
-                    }
-                    break;
-                case 3:
-                    break;
-
-                default:
-                    cout << "Invalid choice" << endl;
-
-            }
-
-        }while (choice != 3);
-
-
-
+        else
+        {
+            throw "This word is not a palindrome.";
+        }
     }
 
     catch (const char *msg)
@@ -66,9 +64,5 @@ int main()
         cout << msg << endl;
     }
 
-
     return 0;
-
-
-
 }
